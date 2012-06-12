024441d82cf39cbbf4d15ac4f62b63f2d2b8a61c
David Barral <david.barral@trabesoluciones.com>
2012-06-12 16:46:55 +0200

    Creating posts from the interface: new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,14 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post, :notice => 'Post created'
+  end
 end
