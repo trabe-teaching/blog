8815e971b101c1ec4e7b960f6bea5e83cafb4bff
Miguel Gonzalez <migonzalvar@gmail.com>
2012-06-25 20:52:36 +0200

    Change new/edit post form to include author field

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :author
 
-  attr_accessible :content, :title, :published
+  attr_accessible :content, :title, :published, :author_id
 
-  validates :title, :content, :presence => true
+  validates :title, :content, :author_id, :presence => true
 
   scope :published, where(:published => true)
   scope :draft, where(:published => false)
