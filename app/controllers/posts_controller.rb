f2278ac0ee2ff1281b448f630834fb201d6e3506
David Barral <david.barral@trabesoluciones.com>
2012-07-26 08:56:21 +0200

    Answer to 2.2. Add pagination. will_paginate version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.published.order('created_at desc')
+    @posts = Post.published.order('created_at desc').paginate(:page => params[:page], :per_page => 2)
   end
 
   def drafts
